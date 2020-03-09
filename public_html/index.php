e2fab63436130b6e7f47e15bbae5bdbd8507d5ce
anytizer <anytizer@gmail.com>
2020-03-09 15:22:14 -0600

    uniform category navigator

--- public_html/index.php
+++ public_html/index.php
@@ -12,14 +12,14 @@ $categories = $nm->categories();
 </head>
 <body>
 <div class="wrapper">
+    <div class="w3-container w3-teal">
+        <h1>Note Taker</h1>
+    </div>
     <form name="notes" method="post" action="save.php" class="w3-white" autocomplete="off">
-        <div class="w3-container w3-teal">
-            <h1>Note Taker</h1>
-        </div>
         <div class="w3-container">
             <label>
                 <p>Enter new note:</p>
-                <textarea name="notes" class="w3-input w3-border w3-border-red" style="height: 200px;" required="required"></textarea>
+                <textarea name="notes" class="w3-input w3-border w3-border-red" style="height: 200px;" required="required" placeholder="Enter notes..."></textarea>
             </label>
             <label>
                 <br/>
@@ -28,23 +28,20 @@ $categories = $nm->categories();
         </div>
         <br/>
     </form>
-<!--    <p>-->
-<!--        <a href="notes.php" class="w3-btn w3-teal">View Notes</a>-->
-<!--        <a href="categories.php" class="w3-btn w3-teal">View Categories</a>-->
-<!--    </p>-->
     <form name="search" method="post" action="search.php" autocomplete="off">
         <label>
-            <input class="w3-input w3-border w3-border-red" type="text" name="query" value=""/>
+            <input class="w3-input w3-border w3-border-red" type="text" name="query" value="" placeholder="Search something..."/>
         </label>
         <label>
             <input class="w3-btn w3-teal w3-input" type="submit" value="Search"/>
         </label>
     </form>
 
-    <div class="categories">
-        <ul>
-            <?php echo implode("\r\n\t\t\t", array_map(array($nm, "_explore_category"), $categories)); ?>
-        </ul>
+    <div class="w3-pale-blue">
+        <h3 class="w3-blue w3-padding">Jump to a Category</h3>
+        <div class="w3-small w3-padding">
+            <ul><?php echo implode("", array_map(array($nm, "_explore_category"), $categories)); ?></ul>
+        </div>
     </div>
 </div>
 </body>
