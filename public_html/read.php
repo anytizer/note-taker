3c0a9ad4ee6b7e15d96b1e0b76d031b4ba72e6ea
anytizer <anytizer@gmail.com>
2020-03-09 15:23:51 -0600

    move to different category

--- public_html/read.php
+++ public_html/read.php
@@ -16,12 +16,7 @@ $categories = $nm->categories();
 </head>
 <body>
 <div class="wrapper wrapper-note">
-    <div class="w3-pale-yellow">
-        <h3 class="w3-yellow w3-padding">Move to a different Category</h3>
-        <div class="w3-small w3-padding">
-            <?php echo implode(", ", array_map(array($nm, "_move_note_category"), $categories)); ?>
-        </div>
-    </div>
+
     <div class="w3-container w3-padding w3-teal">
         <?php echo $note->title; ?>
     </div>
@@ -32,7 +27,22 @@ $categories = $nm->categories();
         <p>
             <a href="edit.php?category=<?php echo $category; ?>&amp;name=<?php echo $name; ?>" class="w3-teal w3-btn">Edit this Note</a>
             <a href="notes.php?category=<?php echo $category; ?>" class="w3-teal w3-btn">Cancel</a>
+            <a href="index.php" class="w3-teal w3-btn">Home</a>
         </p>
+    </div>
+
+    <div class="w3-pale-yellow">
+        <h3 class="w3-yellow w3-padding">Assign a different Category</h3>
+        <div class="w3-small w3-padding">
+            <?php echo implode(", ", array_map(array($nm, "_move_note_category"), $categories)); ?>
+        </div>
+    </div>
+
+    <div class="w3-pale-blue">
+        <h3 class="w3-blue w3-padding">Jump to a Category</h3>
+        <div class="w3-small w3-padding">
+            <ul><?php echo implode("", array_map(array($nm, "_explore_category"), $categories)); ?></ul>
+        </div>
     </div>
 </div>
 </body>
